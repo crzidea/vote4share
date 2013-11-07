c8501eaa62e0a9df48a1ee6542c5d9f8381b3778
crzidea <micro-tech@foxmail.com>
2013-11-07 22:43:30 +0800

    Signed-off-by: crzidea <micro-tech@foxmail.com>

---
+++ public/javascripts/index.js
@@ -0,0 +1,23 @@
+function TrainCtrl($scope) {
+    $scope.trains = [
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133443_st.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133443_st.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133443_st.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133443_st.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133456_s.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133456_s.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133456_s.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133456_s.jpg', link: 'http://sohu.com'}
+    ];
+
+    $scope.ranking = [
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133443_st.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133443_st.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133443_st.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133443_st.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133456_s.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133456_s.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133456_s.jpg', link: 'http://sohu.com'},
+        {name: 'helloname', speaker: 'hellospeaker', votes:123, img: 'Img1133456_s.jpg', link: 'http://sohu.com'}
+    ];
+}
