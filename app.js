680b0e660891ea1f3cb62c62ce4fe58021858bf8
Li Xin <jackxinli@sohu-inc.com>
2013-11-19 11:57:35 +0800

    use cluster.

--- app.js
+++ app.js
@@ -5,34 +5,50 @@ var express = require('express'),
     routes = require('./routes'),
     http = require('http'),
     path = require('path'),
-    config = require('./config');
+    config = require('./config'),
+    cluster = require('cluster'),
+    http = require('http'),
+    numCPUs = require('os').cpus().length,
 
-var app = express()
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    
+    var app = express()
 
 
-// all environments
-app.set('port', process.env.PORT || config.port);
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'views')));
+    // all environments
+    app.set('port', process.env.PORT || config.port);
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'views')));
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+    // development only
+    if ('development' == app.get('env')) {
+        app.use(express.errorHandler());
+    }
+
+    app.get('/', routes.index);
+
+    app.get('/share', routes.share.list);
+    app.post('/share', routes.share.add);
+    app.post('/share/:id/votes', routes.share.votes);
+    app.delete('/share/:id', routes.share.del);
+
+    http.createServer(app).listen(app.get('port'), function() {
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+
 }
-
-app.get('/', routes.index);
-
-app.get('/share', routes.share.list);
-app.post('/share', routes.share.add);
-app.post('/share/:id/votes', routes.share.votes);
-app.delete('/share/:id', routes.share.del);
-
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
