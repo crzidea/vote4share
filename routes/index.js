10437afd560b09e767f242739760042a55c7547c
Li Xin <jackxinli@sohu-inc.com>
2013-11-28 00:35:51 +0800

    remove unused sesssion request.

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@
 exports.index = function(req, res) {
 	if (req.headers['x-real-ip']) {
 		console.log(req.headers['x-real-ip'], req.headers['x-forwarded-for']);
+		req.session.voteAccess = true;
 	}
 	res.sendfile('views/index.html');
-	req.session.voteAccess = true;
 };
 
 exports.share = require('./share');
