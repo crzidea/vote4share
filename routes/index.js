6cb7857ee7cdaf34b924080ae709d19b8bd8dbfb
Li Xin <jackxinli@sohu-inc.com>
2013-11-27 23:48:34 +0800

    typo.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
  */
 exports.index = function(req, res) {
 	if (req.headers['x-real-ip']) {
-		console.log(req.headers['x-realip'], req.headers['X-Forwarded-For']);
+		console.log(req.headers['x-real-ip'], req.headers['x-forwarded-for']);
 	}
 	res.sendfile('views/index.html');
 	req.session.voteAccess = true;
